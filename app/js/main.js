1728cb3b5eba9845ee8b2fe381ce39d7f88cc75d
Mher-777 <mherjan2003@mail.ru>
2020-03-08 17:49:46 +0400

    added section our-company

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,31 @@
 $(function () {
-    
+    let a = 0;
+    $(window).scroll(function () {
+        var oTop = $('.our-company__statistics').offset().top - window.innerHeight;
+        if (a == 0 && $(window).scrollTop() > oTop) {
+            $('.our-company__number').each(function () {
+                var $this = $(this),
+                    countTo = $this.attr('data-count');
+                $({
+                    countNum: $this.text()
+                }).animate({
+                    countNum: countTo
+                },
+                    {
+                        duration: 3000,
+                        easing: 'swing',
+                        step: function () {
+                            $this.text(Math.floor(this.countNum));
+                        },
+                        complete: function () {
+                            $this.text(this.countNum);
+                        }
+
+                    });
+            });
+            a = 1;
+        }
+
+    });
+
 })
